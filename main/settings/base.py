2e758e123a0f82ac289714c700eb5408bf92002d
Ferhat ARMUTLU <ferhat1568armutlu@gmail.com>
2022-07-04 01:05:08 +0300

    add users

--- main/settings/base.py
+++ main/settings/base.py
@@ -33,6 +33,13 @@ INSTALLED_APPS = [
     # Third party apps: 
     'rest_framework',
     'drf_yasg',
+    "rest_framework.authtoken",
+    "dj_rest_auth",
+
+
+    #my apss
+    "users",
+
 ]
 
 MIDDLEWARE = [
@@ -106,3 +113,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES" : [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
+
+REST_AUTH_SERIALIZERS = {
+    
+    'TOKEN_SERIALIZER': 'users.serializers.CustomTokenSerializer',
+    
+}
